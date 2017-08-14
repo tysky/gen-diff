8d6fecfa20c357cf05ed85be888fc65764071343
tysky <dtysky51@gmail.com>
2017-08-14 22:12:33 +0300

    add commander.js simple logic for help information

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
+
 
 console.log('Hello, gendiff!');
