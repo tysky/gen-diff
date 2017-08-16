906f2be51af4033c2ea357890c7f218bed8d5b51
tysky <dtysky51@gmail.com>
2017-08-16 10:23:18 +0300

    update package info

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-/*  import fs from 'fs';
-
-export default () => {
-  console.log('INDEX!!!!!!!!!!!!');
-  // console.log('EXIST!!!!!!!!', fs.existsSync('file.txt'));
-  // console.log(fs.realpathSync('PATH!!!!!!!!!', '/', 'utf8'));
-  const obj = fs.readFileSync('/home/dmitry/my_projects/hexlet_projects/
-              project-lvl2-s117/src/before.json', 'utf8');
-  console.log(obj);
-  console.log(typeof JSON.parse(obj));
-  // console.log(JSON.parse(obj));
-  console.log('FINISH!!!!!!!!!!!!!!!!!!');
-};  */
 import program from 'commander';
 import fs from 'fs';
 import config from '../package.json';
