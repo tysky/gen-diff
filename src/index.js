70dd6c531acc877bc1cc98fde9b0a594c5ae04e2
tysky <dtysky51@gmail.com>
2017-08-16 15:57:54 +0300

    delete old comments

--- src/index.js
+++ src/index.js
@@ -4,11 +4,5 @@ import formatToJS from './formatToJS';
 export default (path1, path2) => {
   const beforeFile = formatToJS(path1);
   const afterFile = formatToJS(path2);
-  // const beforeFile = fs.readFileSync(path1); // get JS object
-  // const firstJSON = JSON.parse(fs.readFileSync(path1)); // get JS object
-  // const afterFile = fs.readFileSync(path2); // get JS object
-  // const secondJSON = JSON.parse(fs.readFileSync(path2)); // get JS object
-  // console.log('BEFORE =', yaml.safeLoad(beforeFile));
-  // console.log('AFTER  =', afterFile);
   return diffObjects(beforeFile, afterFile);
 };
