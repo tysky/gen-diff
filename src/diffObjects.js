cf736f0ddd4902f54001c7f0e1c91e4cbbb6dd79
tysky <dtysky51@gmail.com>
2017-08-17 16:31:04 +0300

    add building nested AST

--- src/diffObjects.js
+++ src/diffObjects.js
@@ -15,24 +15,40 @@ const toString = (ast) => {
 };
 
 // two objects in arguments
-export default (obj1, obj2) => {
+const buildAST = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keysUnion = _.union(keys1, keys2);
 
   const getType = (key) => {
     if (keys1.includes(key) && keys2.includes(key)) {
-      return obj1[key] === obj2[key] ? 'unchanged' : 'changed';
+      // return obj1[key] === obj2[key] ? 'unchanged' : 'changed';
+      return _.isEqual(obj1[key], obj2[key]) ? 'unchanged' : 'changed';
     } else if (keys1.includes(key)) {
       return 'removed';
     }
     return 'added'; // if keys2.includes(key)
   };
 
-  const buildNode = key =>
-    ({ key, oldValue: obj1[key], newValue: obj2[key], type: getType(key) });
+  const buildNode = (key) => {
+    const oldValue = obj1[key];
+    const newValue = obj2[key];
+    if (obj1[key] !== Object && obj2[key] !== Object) {
+      return { key, oldValue, newValue, type: getType(key) };
+    }
+    return {
+      key,
+      oldValue: buildAST(oldValue, newValue),
+      newValue: buildAST(oldValue, newValue),
+      type: getType(key),
+    };
+  };
 
-  const ast = keysUnion.map(buildNode);
+  return keysUnion.map(buildNode);
+};
 
+export default (obj1, obj2) => {
+  const ast = buildAST(obj1, obj2);
+  // console.log('OBJECT==================\n', ast);
   return toString(ast);
 };
