3ecc3a1f6ad501e4395b7025ce9744c62f38e4cb
tysky <dtysky51@gmail.com>
2017-08-18 20:03:28 +0300

    add building AST and toString module to show difference between nested files

--- src/diffObjects.js
+++ src/diffObjects.js
@@ -1,29 +1,15 @@
 import _ from 'lodash';
-
-const toString = (ast) => {
-  const str = ast.reduce((acc, element) => {
-    if (element.type === 'changed') {
-      return `${acc}+ ${element.key}: ${element.newValue}\n- ${element.key}: ${element.oldValue}\n`;
-    } else if (element.type === 'added') {
-      return `${acc}+ ${element.key}: ${element.newValue}\n`;
-    } else if (element.type === 'removed') {
-      return `${acc}- ${element.key}: ${element.oldValue}\n`;
-    }
-    return `${acc}  ${element.key}: ${element.oldValue}\n`; // elememt.type === 'unchanged';
-  }, '');
-  return `{\n${str}}`;
-};
+import toString from './toString';
 
 // two objects in arguments
 const buildAST = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
+  const keys1 = obj1 === undefined ? [] : Object.keys(obj1);
+  const keys2 = obj2 === undefined ? [] : Object.keys(obj2);
   const keysUnion = _.union(keys1, keys2);
 
   const getType = (key) => {
     if (keys1.includes(key) && keys2.includes(key)) {
-      // return obj1[key] === obj2[key] ? 'unchanged' : 'changed';
-      return _.isEqual(obj1[key], obj2[key]) ? 'unchanged' : 'changed';
+      return obj1[key] === obj2[key] ? 'unchanged' : 'changed';
     } else if (keys1.includes(key)) {
       return 'removed';
     }
@@ -31,17 +17,17 @@ const buildAST = (obj1, obj2) => {
   };
 
   const buildNode = (key) => {
-    const oldValue = obj1[key];
-    const newValue = obj2[key];
-    if (obj1[key] !== Object && obj2[key] !== Object) {
-      return { key, oldValue, newValue, type: getType(key) };
+    const oldValue = obj1 === undefined ? undefined : obj1[key];
+    const newValue = obj2 === undefined ? undefined : obj2[key];
+
+    if (oldValue instanceof Object && newValue instanceof Object) {
+      return {
+        key,
+        values: buildAST(oldValue, newValue),
+        hasChildren: true,
+      };
     }
-    return {
-      key,
-      oldValue: buildAST(oldValue, newValue),
-      newValue: buildAST(oldValue, newValue),
-      type: getType(key),
-    };
+    return { key, oldValue, newValue, type: getType(key), hasChildren: false };
   };
 
   return keysUnion.map(buildNode);
@@ -49,6 +35,5 @@ const buildAST = (obj1, obj2) => {
 
 export default (obj1, obj2) => {
   const ast = buildAST(obj1, obj2);
-  // console.log('OBJECT==================\n', ast);
   return toString(ast);
 };
---
+++ src/toString.js
@@ -0,0 +1,36 @@
+const spacing = level => `${'    '.repeat(level)}  `;
+
+const nodeToString = (acc, element, level) => {
+  const toStr = (item) => {
+    if (item instanceof Object) {
+      const json = JSON.stringify(item, null, '');
+      const str = json.slice(1, -1).replace(/["]+/g, '').replace(/[:]/g, ': ');
+      return `{\n${spacing(level + 1)}  ${str}\n${spacing(level)}  }`;
+    // console.log(JSON.stringify({ a: 2 }, null, ' '));
+      // return json.replace(/['"]+/g, '');
+    }
+    return item;
+  };
+  if (element.type === 'changed') {
+    return `${acc}${spacing(level)}+ ${element.key}: ${toStr(element.newValue)}\n${spacing(level)}- ${element.key}: ${toStr(element.oldValue)}\n`;
+  } else if (element.type === 'added') {
+    return `${acc}${spacing(level)}+ ${element.key}: ${toStr(element.newValue)}\n`;
+  } else if (element.type === 'removed') {
+    return `${acc}${spacing(level)}- ${element.key}: ${toStr(element.oldValue)}\n`;
+  }
+  return `${acc}${spacing(level)}  ${element.key}: ${element.oldValue}\n`; // element.type === 'unchanged';
+};
+
+const toString = (ast, level = 0) => {
+  // console.log('AST==========', ast);
+  const str = ast.reduce((acc, element) => {
+    if (element.hasChildren) {
+      return `${acc}${spacing(level)}  ${element.key}: {\n${toString(element.values, level + 1)}${spacing(level)}  }\n`;
+    }
+    return nodeToString(acc, element, level);
+  }, '');
+  return `${str}`;
+};
+const finish = ast => `{\n${toString(ast, 0)}  }`;
+// export default toString;
+export default finish;
