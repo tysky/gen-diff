18eb396bfd4045639becc5e71d4d4a00c64ecf21
tysky <dtysky51@gmail.com>
2017-08-18 20:00:39 +0300

    move parse to default function

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import path from 'path';
 import diffObjects from './diffObjects';
 import parse from './parse';
 
-const getFile = (pathToFile) => {
-  const data = fs.readFileSync(pathToFile, 'utf8');
-  const extension = path.extname(pathToFile, 'utf8');
-  return { data, extension };
-};
 
 export default (path1, path2) => {
-  const obj1 = parse(getFile(path1));
-  const obj2 = parse(getFile(path2));
+  const obj1 = parse({
+    data: fs.readFileSync(path1, 'utf8'),
+    extension: path.extname(path1, 'utf8') });
+  const obj2 = parse({
+    data: fs.readFileSync(path2, 'utf8'),
+    extension: path.extname(path2, 'utf8') });
   return diffObjects(obj1, obj2);
 };
