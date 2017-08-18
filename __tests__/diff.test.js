7004cb14d23735f13759a10364f1d88f848bd258
tysky <dtysky51@gmail.com>
2017-08-18 19:46:51 +0300

    update tests

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -2,32 +2,32 @@ import gendiff from '../src/index';
 
 describe('test difference between flat files', () => {
   const expectedDiff = `{
-  host: hexlet.io
-+ timeout: 20
-- timeout: 50
-- proxy: 123.234.53.22
-+ verbose: true
-}`;
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+  }`;
 
   it('test diff in JSON files', () => {
     // relative path to flat JSON files
-    const beforeJSON = './__tests__/examples/before.json';
-    const afterJSON = './__tests__/examples/after.json';
+    const beforeJSON = './__tests__/fixtures/before.json';
+    const afterJSON = './__tests__/fixtures/after.json';
     expect(gendiff(beforeJSON, afterJSON)).toBe(expectedDiff);
   });
 
   it('test diff in YAML files', () => {
     // relative path to flat YAML files
-    const beforeYAML = './__tests__/examples/before.yml';
-    const afterYAML = './__tests__/examples/after.yml';
+    const beforeYAML = './__tests__/fixtures/before.yml';
+    const afterYAML = './__tests__/fixtures/after.yml';
 
     expect(gendiff(beforeYAML, afterYAML)).toBe(expectedDiff);
   });
 
   it('test diff in INI files', () => {
     // relative path to flat INI files
-    const beforeINI = './__tests__/examples/before.ini';
-    const afterINI = './__tests__/examples/after.ini';
+    const beforeINI = './__tests__/fixtures/before.ini';
+    const afterINI = './__tests__/fixtures/after.ini';
 
     expect(gendiff(beforeINI, afterINI)).toBe(expectedDiff);
   });
@@ -58,19 +58,28 @@ describe('test difference between nested files', () => {
   + group3: {
         fee: 100500
     }
-}`;
+  }`;
+
   it('test diff in JSON files', () => {
     // relative path to nested JSON files
-    const beforeNestedJSON = './__tests__/examples/beforeNested.json';
-    const afterNestedJSON = './__tests__/examples/afterNested.json';
+    const beforeNestedJSON = './__tests__/fixtures/beforeNested.json';
+    const afterNestedJSON = './__tests__/fixtures/afterNested.json';
     expect(gendiff(beforeNestedJSON, afterNestedJSON)).toBe(expectedDiff);
   });
 
   it('test diff in YAML files', () => {
-    // relative path to flat YAML files
-    const beforeNestedYAML = './__tests__/examples/beforeNested.yml';
-    const afterNestedYAML = './__tests__/examples/afterNested.yml';
+    // relative path to nested YAML files
+    const beforeNestedYAML = './__tests__/fixtures/beforeNested.yml';
+    const afterNestedYAML = './__tests__/fixtures/afterNested.yml';
 
     expect(gendiff(beforeNestedYAML, afterNestedYAML)).toBe(expectedDiff);
+  });
+
+  it('test diff in INI files', () => {
+    // relative path to nested INI files
+    const beforeNestedINI = './__tests__/fixtures/beforeNested.ini';
+    const afterNestedINI = './__tests__/fixtures/afterNested.ini';
+
+    expect(gendiff(beforeNestedINI, afterNestedINI)).toBe(expectedDiff);
   });
 });
