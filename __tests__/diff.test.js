be3f030b1db1be7a36599d0a138d6b48f50d88ad
tysky <dtysky51@gmail.com>
2017-08-20 23:16:12 +0300

    update tests

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -138,9 +138,32 @@ Property 'group3' was added with complex value
   });
 });
 
+describe('test the JSON output of the difference between flat files', () => {
+  const expectedDiff = '[{"host":{"type":"unchanged","value":"hexlet.io"}},{"timeout":{"type":"changed","oldValue":50,"newValue":20}},{"proxy":{"type":"removed","oldValue":"123.234.53.22"}},{"verbose":{"type":"added","newValue":true}}]';
+
+  it('test diff in JSON files', () => {
+    const beforeJSON = './__tests__/fixtures/before.json';
+    const afterJSON = './__tests__/fixtures/after.json';
+    expect(gendiff(beforeJSON, afterJSON, 'json')).toBe(expectedDiff);
+  });
+
+  it('test diff in YAML files', () => {
+    const beforeYAML = './__tests__/fixtures/before.yml';
+    const afterYAML = './__tests__/fixtures/after.yml';
+
+    expect(gendiff(beforeYAML, afterYAML, 'json')).toBe(expectedDiff);
+  });
+
+  it('test diff in INI files', () => {
+    const expectedDiffIni = '[{"host":{"type":"unchanged","value":"hexlet.io"}},{"timeout":{"type":"changed","oldValue":"50","newValue":"20"}},{"proxy":{"type":"removed","oldValue":"123.234.53.22"}},{"verbose":{"type":"added","newValue":true}}]';
+    const beforeINI = './__tests__/fixtures/before.ini';
+    const afterINI = './__tests__/fixtures/after.ini';
+    expect(gendiff(beforeINI, afterINI, 'json')).toBe(expectedDiffIni);
+  });
+});
 
 describe('test the JSON output of the difference between nested files', () => {
-  const expectedDiff = '{"common":{"setting1":{"type":"unchanged","value":"Value 1"},"setting2":{"type":"removed","oldValue":"200"},"setting3":{"type":"unchanged","value":true},"setting6":{"type":"removed","oldValue":{"key":"value"}},"setting4":{"type":"added","newValue":"blah blah"},"setting5":{"type":"added","newValue":{"key5":"value5"}}},"group1":{"baz":{"type":"changed","oldValue":"bas","newValue":"bars"},"foo":{"type":"unchanged","value":"bar"}},"group2":{"type":"removed","oldValue":{"abc":"12345"}},"group3":{"type":"added","newValue":{"fee":"100500"}}}';
+  const expectedDiff = '[{"common":[{"setting1":{"type":"unchanged","value":"Value 1"}},{"setting2":{"type":"removed","oldValue":"200"}},{"setting3":{"type":"unchanged","value":true}},{"setting6":{"type":"removed","oldValue":{"key":"value"}}},{"setting4":{"type":"added","newValue":"blah blah"}},{"setting5":{"type":"added","newValue":{"key5":"value5"}}}]},{"group1":[{"baz":{"type":"changed","oldValue":"bas","newValue":"bars"}},{"foo":{"type":"unchanged","value":"bar"}}]},{"group2":{"type":"removed","oldValue":{"abc":"12345"}}},{"group3":{"type":"added","newValue":{"fee":"100500"}}}]';
 
   it('test diff in JSON files', () => {
     const beforeNestedJSON = './__tests__/fixtures/beforeNested.json';
