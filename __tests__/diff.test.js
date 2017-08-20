64b7130c20f31d6824fb9c10e551dac36ceab798
tysky <dtysky51@gmail.com>
2017-08-20 15:54:59 +0300

    update tests

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,6 +1,6 @@
 import gendiff from '../src/index';
 
-describe('test difference between flat files', () => {
+describe('test the difference between flat files', () => {
   const expectedDiff = `{
     host: hexlet.io
   + timeout: 20
@@ -33,7 +33,7 @@ describe('test difference between flat files', () => {
   });
 });
 
-describe('test difference between nested files', () => {
+describe('test the difference between nested files', () => {
   const expectedDiff = `{
     common: {
         setting1: Value 1
@@ -84,7 +84,34 @@ describe('test difference between nested files', () => {
   });
 });
 
-describe('test difference between files in flat mode', () => {
+describe('test the difference between flat files in plain mode', () => {
+  const expectedDiff = `Property 'timeout' was updated. From '50' to '20'
+Property 'proxy' was removed
+Property 'verbose' was added with value: true
+`;
+
+  it('test diff in JSON files', () => {
+    const beforeJSON = './__tests__/fixtures/before.json';
+    const afterJSON = './__tests__/fixtures/after.json';
+    expect(gendiff(beforeJSON, afterJSON, 'plain')).toBe(expectedDiff);
+  });
+
+  it('test diff in YAML files', () => {
+    const beforeYAML = './__tests__/fixtures/before.yml';
+    const afterYAML = './__tests__/fixtures/after.yml';
+
+    expect(gendiff(beforeYAML, afterYAML, 'plain')).toBe(expectedDiff);
+  });
+
+  it('test diff in INI files', () => {
+    const beforeINI = './__tests__/fixtures/before.ini';
+    const afterINI = './__tests__/fixtures/after.ini';
+    expect(gendiff(beforeINI, afterINI, 'plain')).toBe(expectedDiff);
+  });
+});
+
+
+describe('test the difference between nested files in plain mode', () => {
   const expectedDiff = `Property 'common.setting2' was removed
 Property 'common.setting6' was removed
 Property 'common.setting4' was added with value: blah blah
@@ -108,5 +135,26 @@ Property 'group3' was added with complex value
     const beforeNestedINI = './__tests__/fixtures/beforeNested.ini';
     const afterNestedINI = './__tests__/fixtures/afterNested.ini';
     expect(gendiff(beforeNestedINI, afterNestedINI, 'plain')).toBe(expectedDiff);
+  });
+});
+
+
+describe('test the JSON output of the difference between nested files', () => {
+  const expectedDiff = '{"common":{"setting1":{"type":"unchanged","value":"Value 1"},"setting2":{"type":"removed","oldValue":"200"},"setting3":{"type":"unchanged","value":true},"setting6":{"type":"removed","oldValue":{"key":"value"}},"setting4":{"type":"added","newValue":"blah blah"},"setting5":{"type":"added","newValue":{"key5":"value5"}}},"group1":{"baz":{"type":"changed","oldValue":"bas","newValue":"bars"},"foo":{"type":"unchanged","value":"bar"}},"group2":{"type":"removed","oldValue":{"abc":"12345"}},"group3":{"type":"added","newValue":{"fee":"100500"}}}';
+
+  it('test diff in JSON files', () => {
+    const beforeNestedJSON = './__tests__/fixtures/beforeNested.json';
+    const afterNestedJSON = './__tests__/fixtures/afterNested.json';
+    expect(gendiff(beforeNestedJSON, afterNestedJSON, 'json')).toBe(expectedDiff);
+  });
+  it('test diff in YAML files', () => {
+    const beforeNestedYAML = './__tests__/fixtures/beforeNested.yml';
+    const afterNestedYAML = './__tests__/fixtures/afterNested.yml';
+    expect(gendiff(beforeNestedYAML, afterNestedYAML, 'json')).toBe(expectedDiff);
+  });
+  it('test diff in INI files', () => {
+    const beforeNestedINI = './__tests__/fixtures/beforeNested.ini';
+    const afterNestedINI = './__tests__/fixtures/afterNested.ini';
+    expect(gendiff(beforeNestedINI, afterNestedINI, 'json')).toBe(expectedDiff);
   });
 });
